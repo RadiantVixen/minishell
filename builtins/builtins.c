0f7f6b96f1a7dde009cf035ade21cf564d501adc
Aicha Atki <aatki@e1r10p8.1337.ma>
2023-04-17 00:57:23 +0000

    hfghkf

--- builtins/builtins.c
+++ builtins/builtins.c
@@ -6,7 +6,7 @@
 /*   By: aatki <aatki@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 05:24:12 by aatki             #+#    #+#             */
-/*   Updated: 2023/04/17 00:34:25 by aatki            ###   ########.fr       */
+/*   Updated: 2023/04/17 00:57:02 by aatki            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,78 +50,84 @@ char	**equal(char *s, char c)
 
 void	ft_export(char **env, char **arg, int fd)
 {
-	// int		i;
-	// char	**export;
-	// char	**sp;
-	// char	**sp2;
+// 	int		i;
+// 	char	**export;
+// 	char	**sp;
+// 	char	**sp2;
 
-	// i = 0;
-	// int j=0;
-	// if(arg)
-	// {
-	// 	sp = equal(arg, '=');
-	// 	if(sp[0][0]=='$')
-	// 	{
-	// 		j=1;
-	// 		sp[0]+=1;
-	// 		arg+=1;
-	// 	}
-	// 	if(here(env,sp[0]))
-	// 	{
-	// 		if(j==1)
-	// 			ft_error("noooo");
-	// 		else
-	// 		{
-	// 			if(sp[0][ft_strlen(sp[0])]=='=')
-	// 				return;
-	// 			else
-	// 			{
-	// 				ft_unset(env,sp[0]);
-	// 				export = ft_envoo(env,arg);
-	// 				//ft_free(env);
-	// 				env=ft_envoo(export,NULL);
-	// 			}
-	// 		}
-	// 	}
-	// 	else
-	// 	{
-	// 		if(sp[0][ft_strlen(sp[0])]=='=')
-	// 		{
-	// 			if(j==1)
-	// 				ft_error("noooo");
-	// 			else
-	// 			{
-	// 				export = ft_envoo(env,arg);
-	// 				//ft_free(env);
-	// 				env=ft_envoo(export,NULL);
-	// 			}
-	// 		}
-	// 		else
-	// 		{
-	// 			if(j==1)
-	// 				return;
-	// 			else
-	// 				export = ft_envoo(env,arg);
-	// 		}
-	// 	}
-	// 	ft_env(export,NULL,1);
-	// }
-	// else
-	// {
-	// 	export = ft_envoo(env,arg);
-	// 	sort_env(export);
-	// 	dup2(fd, 1);
-	// 	// ft_env(env,NULL,1);
-	// 	// exit(0);
-	// 	while (export[i])
-	// 	{
-	// 		sp2 = equal(export[i], '=');
-	// 		printf("declare -x %s\"%s\"\n", sp2[0], sp2[1]);
-	// 		free(sp2[0]);
-	// 		free(sp2);
-	// 		i++;
-	// 	}
-	// }
+// 	i = 0;
+// 	int k=0;
+// 	int j=0;
+	
+// 	if(*arg)
+// 	{
+// 		while(arg[k])
+// 		{
+// 				sp = equal(arg[k], '=');
+// 			if(sp[0][0]=='$')
+// 			{
+// 				j=1;
+// 				sp[0]+=1;
+// 				arg[k]+=1;
+// 			}
+// 			if(here(env,sp[0]))
+// 			{
+// 				if(j==1)
+// 					ft_error("noooo");
+// 				else
+// 				{
+// 					if(sp[0][ft_strlen(sp[0])]=='=')
+// 						return;
+// 					else
+// 					{
+// 						ft_unset(env,sp[0]);
+// 						export = ft_envoo(env,arg[k]);
+// 						//ft_free(env);
+// 						env=ft_envoo(export,NULL);
+// 					}
+// 				}
+// 			}
+// 			else
+// 			{
+// 				if(sp[0][ft_strlen(sp[0])]=='=')
+// 				{
+// 					if(j==1)
+// 						ft_error("noooo");
+// 					else
+// 					{
+// 						export = ft_envoo(env,arg[k]);
+// 						//ft_free(env);
+// 						env=ft_envoo(export,NULL);
+// 					}
+// 				}
+// 				else
+// 				{
+// 					if(j==1)
+// 						return;
+// 					else
+// 						export = ft_envoo(env,arg[k]);
+// 				}
+// 			}
+// 			k++;
+// 		}
+// 		ft_env(export,NULL,1);
+// 	}
+// 	else
+// 	{
+// 		export = ft_envoo(env,arg);
+// 		sort_env(export);
+// 		dup2(fd, 1);
+// 		// ft_env(env,NULL,1);
+// 		// exit(0);
+// 		while (export[i])
+// 		{
+// 			sp2 = equal(export[i], '=');
+// 			printf("declare -x %s\"%s\"\n", sp2[0], sp2[1]);
+// 			free(sp2[0]);
+// 			free(sp2);
+// 			i++;
+// 		}
+// 	}
 }
 
 void	ft_env(char **env, char **arg, int fd)
@@ -129,23 +135,30 @@ void	ft_env(char **env, char **arg, int fd)
 	int		i;
 	char	**sp;
 
-	// i = 0;
-	// dup2(fd, 1);
-	// if (arg)
-	// 	sp = equal(arg, '=');
-	// if (arg && !here(env, sp[0]))
-	// 	ft_error("No such file or directory");
-	// while (env[i])
-	// {
-	// 	if (arg && here(env, sp[0]) && !ft_strncmp(env[i], sp[0],
-	// 			ft_strlen(sp[0])))
-	// 	{
-	// 		printf("%s\n", arg);
-	// 		i++;
-	// 	}
-	// 	printf("%s\n", env[i]);
-	// 	i++;
-	// }
+
+	dup2(fd, 1);
+	int j=0;
+	while(arg[j])
+	{
+		i=0;
+		if (arg[j])
+			sp = equal(arg[j], '=');
+		if (arg[j] && !here(env, sp[0]))
+			ft_errorb("No such file or directory");
+		while (env[i])
+		{
+			if (arg[j] && here(env, sp[0]) && !ft_strncmp(env[i], sp[0],
+					ft_strlen(sp[0])))
+			{
+				printf("%s\n", arg[j]);
+				i++;
+			}
+			printf("%s\n", env[i]);
+			i++;
+		}
+		ft_free(sp);
+		j++;
+	}
 }
 
 void	ft_pwd(int fd)
@@ -163,22 +176,28 @@ void	ft_unset(char **env, char **arg)
 {
 	int	i;
 
-	i = 0;
-	// while ((env[i]))
-	// {
-	// 	if (arg && !ft_strncmp(env[i], arg, ft_strlen(arg)))
-	// 		break ;
-	// 	i++;
-	// }
-	// if (!env[i])
-	// 	return ;
-	// while ((env[i + 1]))
-	// {
-	// 	printf("vjkfdbh\n");
-	// 	//free(env[i]);
-	// 	env[i] = ft_strdup(env[i + 1]);
-	// 	i++;
-	// }
-	// env[i] = NULL;
-	// ft_env(env, NULL, 1);
+	int j=0;
+	while(arg[j])
+	{
+		i=0;
+		while ((env[i]))
+		{
+			if (arg && !ft_strncmp(env[i], arg[j], ft_strlen(arg[j])))
+				break ;
+			i++;
+		}
+		if (!env[i])
+			return ;
+		while ((env[i + 1]))
+		{
+			printf("vjkfdbh\n");
+			//free(env[i]);
+			env[i] = ft_strdup(env[i + 1]);
+			i++;
+		}
+		env[i] = NULL;
+		j++;
+	}
+	
+	ft_env(env, NULL, 1);
 }
